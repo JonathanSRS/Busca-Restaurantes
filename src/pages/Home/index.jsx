b5925ca7c29d14f54ae7e5bbde786abde891a54c
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-10-25 13:47:47 -0300

    Adicionando: logo; Criando: input de pesquisar; Instalando: @material para design do input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import TextField, { Input } from "@material/react-text-field";
 
-import { Container } from "./styled";
+import logo from './../../assets/logo.svg'
 
-const Home = () => <Container>Hellow word</Container>
+import { Container, Search } from "./styled";
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState('')
+
+    return(
+        <Container>
+            <Search>
+                <img src={logo} alt="Logo Restaurante"/>
+                <TextField
+                label='Pequisar'
+
+                // onTrailingIconSelect={() => this.setState({value: ''})}
+                // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+            >
+                <Input
+                value={inputValue}
+                onChange={(e) =>  setInputValue(e.target.value)}/>
+            </TextField>
+            </Search>
+        </Container>
+    );
+};
 
 export default Home;

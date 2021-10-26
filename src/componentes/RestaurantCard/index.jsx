6f0c94011d67e36287f8a5e11cb0e061e3935830
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-10-26 09:53:14 -0300

    Instalando: ReactStars pra avaliação do restaurante; Estilizando: imagens

--- src/componentes/RestaurantCard/index.jsx
+++ src/componentes/RestaurantCard/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-import { Address, Restaurant, RestaurantInfo, Title } from "./styles";
+import { Address, Restaurant, RestaurantInfo, RestaurantPhoto, Title } from "./styles";
+import ReactStars from 'react-rating-stars-component';
+
+import pizza from './../../assets/pizza_640px.jpg'
 
 const RestaurantCard = () => 
 <Restaurant>
     <RestaurantInfo>
         <Title>Nome do Restaurante</Title>
-        <p>avaliação</p>
+        <ReactStars count={5} isHalf value={4} edit={false} activeColor='#e7711c'/>
         <Address>endereço</Address>
     </RestaurantInfo>
+    <RestaurantPhoto src={pizza} />
 </Restaurant>;
 
 export default RestaurantCard;
--- src/componentes/RestaurantCard/styles.js
+++ src/componentes/RestaurantCard/styles.js
@@ -35,3 +35,10 @@ export const Title = styled.span`
     line-height: 19px;
     margin-bottom: 10px;
 `;
+
+export const RestaurantPhoto = styled.img`
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    border-radius: 6px;
+`;
